8d043d6c78ce41d395c8763910203cc7a7f7a1b0
himanshu-10 <himanshusrivastava5390@gmail.com>
2023-10-02 04:12:56 +0530

    make a component and return

--- 01basic/src/App.jsx
+++ 01basic/src/App.jsx
@@ -1,11 +1,15 @@
 
 
-
+import Demo from "./demo"
 function App() {
  
 
   return (
-    <h1>Hello React via Vite || HS </h1>
+    <>
+      <h1>Hello React via Vite || HS </h1>
+      <Demo/>
+    </>
+    
   )
 }
 
---
+++ 01basic/src/Demo.jsx
@@ -0,0 +1,9 @@
+
+function demo(){
+    return (
+        <h1> hello React </h1>
+    )
+        
+    
+}
+export default demo;
