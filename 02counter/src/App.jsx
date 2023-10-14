365c03aa74fe2f8732e684fb0c9c026c4e2f4dc8
himanshu-10 <himanshusrivastava5390@gmail.com>
2023-10-14 20:57:45 +0530

    first

---
+++ 02counter/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  
+  let [counter,setCounter] = useState(15);
+  
+
+  const incVal = () =>{  
+    setCounter(counter+1);
+  }
+
+  const desVal = () =>{
+    setCounter(counter -1);
+  }
+
+  return (
+    <>
+      <h2>Counter : {counter}</h2>
+
+      <button
+        onClick={incVal}      
+      >Increase Value </button>
+
+      <br />
+
+      <button
+        onClick={desVal}      
+      >Decrease Value</button>
+       
+    </>
+  )
+}
+
+export default App
